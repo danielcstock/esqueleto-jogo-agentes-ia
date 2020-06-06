eb3926ad9bfc6d35891c6fbff2edbf294601160f
Daniel Cunha Stock <danielcstock12@gmail.com>
2020-06-06 17:31:46 -0300

    Método DFS, DFS Limitado e DFS Iterativo implementados.

--- agentes/dfs_iterativo.py
+++ agentes/dfs_iterativo.py
@@ -6,6 +6,7 @@ import time
 
 class AgenteDFSIterativo(AgenteDFS):
     def tracarTrajeto(self, percepcao_mundo):
+        limite = int(len(percepcao_mundo)**2/2)
         x = 0
         y = 0
         s_x = 0
@@ -31,44 +32,51 @@ class AgenteDFSIterativo(AgenteDFS):
         no_anterior = list()
         no_anterior.append(self.grafo[0])
         while not has_solution:
-            if s in no.ligacoes:
-                while no.ligacoes[0] != s:
-                    no.ligacoes.pop(0)
-                lista_solucoes[-1].append(no.ligacoes[0])
-                has_solution = True
-            elif no.qtdLigacoes == 0:
-                no = bifurcacoes.pop()
-                lista_solucoes.pop()
-            elif no.qtdLigacoes == 0:
-                no = no_anterior.pop()
-                no.nos.pop(0)
-
-            elif no.qtdLigacoes == 1:
-                if no.ligacoes[0] != no.getPosicao() and no.ligacoes[0] not in lista_solucoes[-1]:
+            if limite >= len(lista_solucoes):
+                if s in no.ligacoes:
+                    while no.ligacoes[0] != s:
+                        no.ligacoes.pop(0)
                     lista_solucoes[-1].append(no.ligacoes[0])
+                    has_solution = True
+                elif no.qtdLigacoes == 0:
+                    no = bifurcacoes.pop()
+                    lista_solucoes.pop()
+                elif no.qtdLigacoes == 0:
+                    no = no_anterior.pop()
+                    no.nos.pop(0)
+                elif no.qtdLigacoes == 1:
+                    if no.ligacoes[0] != no.getPosicao() and no.ligacoes[0] not in lista_solucoes[-1]:
+                        lista_solucoes[-1].append(no.ligacoes[0])
+                        no_anterior.append(no)
+                        no = no.nos[0]
+                        if no_anterior[-1] != self.grafo[0] and no_anterior[-1] in no.nos:
+                            no.nos.remove(no_anterior[-1])
+                            no.ligacoes.remove(no_anterior[-1].getPosicao())
+                            no.qtdLigacoes -= 1
+                    else:
+                        no = no_anterior.pop()
+                elif no.qtdLigacoes > 1:
+                    lista_solucoes.append(list())
+                    for p in lista_solucoes[-2]:
+                        lista_solucoes[-1].append(p)
+                    bifurcacoes.append(no)
+                    lista_solucoes[-1].append(no.ligacoes[0])
+                    no.qtdLigacoes -= 1
+                    no.ligacoes.remove(lista_solucoes[-1][-1])
                     no_anterior.append(no)
                     no = no.nos[0]
-                    if no_anterior[-1] != self.grafo[0] and no_anterior[-1] in no.nos:
-                        no.nos.remove(no_anterior[-1])
-                        no.ligacoes.remove(no_anterior[-1].getPosicao())
-                        no.qtdLigacoes -= 1
-                else:
-                    no = no_anterior.pop()
-            elif no.qtdLigacoes > 1:
-                lista_solucoes.append(list())
-                for p in lista_solucoes[-2]:
-                    lista_solucoes[-1].append(p)
-                bifurcacoes.append(no)
-                lista_solucoes[-1].append(no.ligacoes[0])
-                no.qtdLigacoes -= 1
-                no.ligacoes.remove(lista_solucoes[-1][-1])
-                no_anterior.append(no)
-                no = no.nos[0]
-                no.nos.remove(no_anterior[-1])
-                no.ligacoes.remove(no_anterior[-1].getPosicao())
-                no.qtdLigacoes -= 1
-                no_anterior[-1].nos.pop(0)
-                
+                    no.nos.remove(no_anterior[-1])
+                    no.ligacoes.remove(no_anterior[-1].getPosicao())
+                    no.qtdLigacoes -= 1
+                    no_anterior[-1].nos.pop(0)
+            else:
+                limite += (len(percepcao_mundo)/10)
+                lista_solucoes = [list()]
+                lista_solucoes[0].append(self.grafo[0].getPosicao())
+                no = self.grafo[0]
+                bifurcacoes = [no]
+                no_anterior = list()
+                no_anterior.append(self.grafo[0])
         self.definirSolucao(lista_solucoes, s)
     
     def escolherProximaAcao(self):
